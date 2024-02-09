e471298d1836059df4a4b01c2a51cb6fbfeb4c35
tryggvi <tryggvi2004@gmail.com>
2024-02-09 00:21:18 +0000

    Updated structure

---
+++ src/UserInterface.java
@@ -0,0 +1,7 @@
+public class UserInterface {
+    public void show(String greeting) {
+        System.out.println("*******************");
+        System.out.println(greeting);
+        System.out.println("*******************");
+    }
+}
