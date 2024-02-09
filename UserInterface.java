eb2758494049db4ef4e3d142ff40a7aed834825e
tryggvi <tryggvi2004@gmail.com>
2024-02-09 00:28:04 +0000

    replaced reapeating programs

--- UserInterface.java
+++
@@ -1,7 +0,0 @@
-public class UserInterface {
-    public void show(String greeting) {
-        System.out.println("*******************");
-        System.out.println(greeting);
-        System.out.println("*******************");
-    }
-}
